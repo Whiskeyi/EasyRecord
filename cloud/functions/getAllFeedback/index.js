6a87fbdf96893f13ae3f834efd024a9f269f6c78
Whiskeyi <825906196@qq.com>
2023-05-19 15:52:07 +0800

    feat: change sort desc

--- cloud/functions/getAllFeedback/index.js
+++ cloud/functions/getAllFeedback/index.js
@@ -7,13 +7,13 @@ cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
 
-  // 获取feedback集合中匹配openid的所有记录
+  // 获取feedback集合中匹配openid的所有记录,根据时间倒序排序
   const db = cloud.database()
+  const _ = db.command
   const feedback = db.collection('feedback')
   const res = await feedback.where({
     openid: wxContext.OPENID
-  }).get()
-
+  }).orderBy('createTime', 'desc').get()
 
   return {
     res
